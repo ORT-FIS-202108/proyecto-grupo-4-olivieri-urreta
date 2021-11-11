2892b4d4aac4b2dfbaf732130781d8b1314c89be
Diego Urreta <diegouuy@gmail.com>
2021-11-11 19:18:29 -0300

    Creacion de archivos de clase Sistema (.mjs y .test.js)

---
+++ src/dominio/sistema.test.js
@@ -0,0 +1,8 @@
+// import Sistema from './sistema.mjs';
+
+// describe('tests de función Login', () => {
+//     test('datos vacios', () => {
+//         l
+//         expect(login)
+//     });
+// });
