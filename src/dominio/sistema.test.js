ed1e2c8b8f9f262670cda4e46fb99a9f41f322f4
Diego Urreta <diegouuy@gmail.com>
2021-11-13 12:16:07 -0300

    Se agregan tests para registro de usuario en sistema.test.js

--- src/dominio/sistema.test.js
+++ src/dominio/sistema.test.js
@@ -1,7 +1,24 @@
+/* eslint-disable max-len */
 import Sistema from './sistema.mjs';
 
-describe('tests de función Registro', () => {
-  test('login con datos vacios', () => {
-    expect(Sistema.iniciarSesion('', '')).toBe('');
+describe('tests de registroUsuario', () => {
+  const sistema = new Sistema();
+  test('registro con usaurio y password vacios', () => {
+    expect(sistema.registroUsuario('', '')).toBe(false);
+  });
+  test('registro con usuario vacio', () => {
+    expect(sistema.registroUsuario('', 'abcd')).toBe(false);
+  });
+  test('registro con formato de usuario inválido', () => {
+    expect(sistema.registroUsuario('nombreusuario', 'abcd')).toBe(false);
+  });
+  test('registro con password vacio', () => {
+    expect(sistema.registroUsuario('user@mail.com', '')).toBe(false);
+  });
+  test('registro válido', () => {
+    expect(sistema.registroUsuario('user@mail.com', 'abcd')).toBe(true);
+  });
+  test('registro con usaurio ya existente', () => {
+    expect(sistema.registroUsuario('user@mail.com', 'fghi')).toBe(false);
   });
 });
