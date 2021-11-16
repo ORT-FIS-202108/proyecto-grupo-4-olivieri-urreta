2f31cab95bbd2f5269e86cb41a456cb8c6f831d3
Diego Urreta <diegouuy@gmail.com>
2021-11-16 17:47:01 -0300

    Se hacen cambios a funciones de clase Gasto

--- src/dominio/gasto.mjs
+++ src/dominio/gasto.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 let proxId = 1; // Contador para el próximo Id.
 /**
  * Clase que maneja las funciones de los gastos.
@@ -5,26 +6,38 @@ let proxId = 1; // Contador para el próximo Id.
 export default class Gasto {
   /**
      * Constructor de la clase Gasto.
-     * @param {string} nombre
-     * @param {string} descripcion
-     * @param {int} monto
-     * @param {Date} fecha
-     * @param {string} categoria
+     * @param {string} nombre Nombre que describe el gasto.
+     * @param {Number} monto Importe del gasto.
+     * @param {Date} fecha Fecha que se realizó el gasto.
+     * @param {Number} categoria Una de las categorías del listado de categorías de Sistema.
+     * @param {Number} idusuario Id del usuario que registró el gasto.
+     * @property {Number} id Id único del gasto (auotgenerado).
+     * @return {Gasto} Retorna el gasto creado.
      */
-  constructor(nombre, descripcion, monto, fecha, categoria) {
+  constructor(nombre, monto, fecha, categoria, idusuario) {
     this.nombre = nombre;
-    this.descripcion = descripcion;
     this.monto = monto;
     this.fecha = fecha;
     this.categoria = categoria;
+    this.idUsuario = idusuario;
     this.id = proxId++;
+    return this;
   }
   /**
    * Valida los datos ingresados para un gasto.
-   * @param {Gasto} gasto
-   * @return {Gasto}
+   * @param {string} nombre Recibe el nombre.
+   * @param {Numbre} monto Recibe el monto del gasto.
+   * @return {string} Retorna el mensaje 'Datos válidos' si el monto y el nombre son válidos,
+   * y sino retorna un mensaje con error.
    */
-  validarDatos(gasto) {
-    return gasto;
+  static validarDatos(nombre, monto) {
+    let mensaje = 'Datos válidos';
+    if (isNaN(monto) || monto <= 0) {
+      mensaje = 'El monto ingresado no es válido. Ingrese un número mayor que 0.';
+    }
+    if (nombre.length < 2) {
+      mensaje = 'El nombre ingresado no es válido. Ingrese un nombre de al menos 2 letras.';
+    }
+    return mensaje;
   }
 }
